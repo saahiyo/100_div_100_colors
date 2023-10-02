34da17796c535f513bc3dceee999a8c0cc9cc23a
Shakir Ansari <linkg518@gmail.com>
2023-10-02 16:19:02 +0530

    Added little animations

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-        // JavaScript code to create 100 div elements with random colors
-        function getRandomColor() {
-            const letters = '0123456789ABCDEF';
-            let color = '#';
-            for (let i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
-            }
-            return color;
-        }
+    let h1 = document.querySelector("h1");
+// JavaScript code to create 100 div elements with random colors
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
-        for (let i = 0; i < 102; i++) {
-            const div = document.createElement('div');
-            div.classList.add('box');
-            div.style.backgroundColor = getRandomColor(); // Set a random background color
-            document.body.appendChild(div);
-        }
+for (let i = 0; i < 102; i++) {
+    const div = document.createElement('div');
+    div.classList.add('box');
+    div.style.backgroundColor = getRandomColor(); // Set a random background color
+    document.body.appendChild(div);
+}
+gsap.from(".box",{
+ x:80,
+ opacity:0,
+ stagger:0.5,
+ duration:1
+})
+gsap.from("h1",{
+ y:100,
+ stagger:0.5,
+ duration:1,
+ opacity:0
+})
